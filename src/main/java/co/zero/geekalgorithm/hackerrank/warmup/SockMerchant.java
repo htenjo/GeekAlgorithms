1da9ef9e1b9f4c6a361bf7dea68a42e0b680c1b2
Hernan <zerovirus23@gmail.com>
2018-10-03 20:48:05 -0500

    Solving the SockMerchant problem with sets.

--- src/main/java/co/zero/geekalgorithm/hackerrank/warmup/SockMerchant.java
+++ src/main/java/co/zero/geekalgorithm/hackerrank/warmup/SockMerchant.java
@@ -1,8 +1,10 @@
 package co.zero.geekalgorithm.hackerrank.warmup;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * John works at a clothing store. He has a large pile of socks that he must pair by color for sale.
@@ -17,9 +19,11 @@ public class SockMerchant {
 
     public static void main(String[] args) {
         //int sockColors[] = {1,2,1,2,1,3,2};
-        int sockColors[] = {10,20,20,10,10,30,50,10,20};
-        int pairs = sockMerchantWithMaps(sockColors.length, sockColors);
-        System.out.println("pairs = " + pairs);
+        int sockColors[] = {10,20,20,10,10,30,50,10,20, 20, 20, 20};
+        int pairsWithMaps = sockMerchantWithMaps(sockColors.length, sockColors);
+        int pairsWithSets = sockMerchantWithSets(sockColors.length, sockColors);
+        System.out.println("pairsWithMaps = " + pairsWithMaps);
+        System.out.println("pairsWithSets = " + pairsWithSets);
     }
 
     /**
@@ -41,6 +45,27 @@ public class SockMerchant {
 
         for (Entry<Integer, Integer> entry : counter.entrySet()) {
             pairs += entry.getValue() / 2;
+        }
+
+        return pairs;
+    }
+
+    /**
+     * Solution using Sets (Better than using maps)
+     * Time complexity ~ O(N)
+     * Space complexity ~ O(N)
+     */
+    private static int sockMerchantWithSets(int n, int[] sockColors) {
+        Set<Integer> colors = new HashSet<>();
+        int pairs = 0;
+
+        for (int color : sockColors) {
+            if (colors.contains(color)) {
+                pairs++;
+                colors.remove(color);
+            } else {
+                colors.add(color);
+            }
         }
 
         return pairs;
